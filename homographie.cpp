176a258b464804bd7a870dda1e0b6a54ef6cc482
Lucas Broux <lucas-broux@orange.fr>
2017-11-08 12:04:02 +0100

    Modif

--- homographie.cpp
+++ homographie.cpp
@@ -1,60 +1,114 @@
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/features2d/features2d.hpp>
-#include <opencv2/calib3d/calib3d.hpp>
-#include <opencv2/imgproc.hpp>
-
+#include <opencv2/sfm.hpp>
+#include <opencv2/viz.hpp>
+#include <opencv2/calib3d.hpp>
+#include <opencv2/core.hpp>
 #include <iostream>
+#include <fstream>
+#include <string>
 using namespace std;
 using namespace cv;
-
-int main()
+using namespace cv::sfm;
+static void help() {
+	cout
+		<< "\n------------------------------------------------------------------------------------\n"
+		<< " This program shows the multiview reconstruction capabilities in the \n"
+		<< " OpenCV Structure From Motion (SFM) module.\n"
+		<< " It reconstruct a scene from a set of 2D images \n"
+		<< " Usage:\n"
+		<< "        example_sfm_scene_reconstruction <path_to_file> <f> <cx> <cy>\n"
+		<< " where: path_to_file is the file absolute path into your system which contains\n"
+		<< "        the list of images to use for reconstruction. \n"
+		<< "        f  is the focal lenght in pixels. \n"
+		<< "        cx is the image principal point x coordinates in pixels. \n"
+		<< "        cy is the image principal point y coordinates in pixels. \n"
+		<< "------------------------------------------------------------------------------------\n\n"
+		<< endl;
+}
+int getdir(const string _filename, vector<string> &files)
 {
-	Mat I1 = imread("../IMG_0045.JPG", CV_LOAD_IMAGE_GRAYSCALE);
-	Mat I2 = imread("../IMG_0046.JPG", CV_LOAD_IMAGE_GRAYSCALE);
-	//Mat I2 = imread("../IMG_0046r.JPG", CV_LOAD_IMAGE_GRAYSCALE);
-	//namedWindow("I1", 1);
-	//namedWindow("I2", 1);
-	//imshow("I1", I1);
-	//imshow("I2", I2);
-
-	vector<KeyPoint> m1, m2;
-	Mat J1, J2;
-	Ptr<AKAZE> akaze = AKAZE::create();
-	akaze->detectAndCompute(I1, noArray(), m1, J1);
-	akaze->detectAndCompute(I2, noArray(), m2, J2);
-
-	Mat II1, II2;
-	drawKeypoints(I1, m1, II1, Scalar::all(-1), 4);
-	drawKeypoints(I2, m2, II2, Scalar::all(-1), 4);
-	//imshow("I1", II1);
-	//imshow("I2", II2);
-
-	BFMatcher matcher(NORM_HAMMING);
-	vector< vector<DMatch> > nn_matches;
-	matcher.knnMatch(J1, J2, nn_matches, 2);
-
-	Mat MATCHES;
-	drawMatches(I1, m1, I2, m2, nn_matches, MATCHES);
-	//imshow("MATCHES", MATCHES);
-
-	vector<Point2f> matched1, matched2;
-	for (size_t i = 0; i < nn_matches.size(); i++) {
-		DMatch first = nn_matches[i][0];
-		matched1.push_back(m1[first.queryIdx].pt);
-		matched2.push_back(m2[first.trainIdx].pt);
+	ifstream myfile(_filename.c_str());
+	if (!myfile.is_open()) {
+		cout << "Unable to read file: " << _filename << endl;
+		exit(0);
 	}
-
-	Mat H = findHomography(matched1, matched2, CV_RANSAC);
-	
-	Mat K(I1.rows, 2*I1.cols,  CV_8U);
-	warpPerspective(I2, K, H, K.size(), WARP_INVERSE_MAP);
-
-	for (int i = 0; i < I1.rows; i++) {
-		for (int j = 0; j < I1.cols; j++) {
-			K.at<uchar>(i, j) = I1.at<uchar>(i, j);
-		}
+	else {
+		;
+		size_t found = _filename.find_last_of("/\\");
+		string line_str, path_to_file = _filename.substr(0, found);
+		while (getline(myfile, line_str))
+			files.push_back(path_to_file + string("/") + line_str);
 	}
-	imshow("K", K);
-	waitKey(0);
+	return 1;
+}
+int main(int argc, char* argv[])
+{
+	// Read input parameters
+	if (argc != 5)
+	{
+		help();
+		exit(0);
+	}
+	// Parse the image paths
+	vector<string> images_paths;
+	getdir(argv[1], images_paths);
+	// Build instrinsics
+	float f = atof(argv[2]),
+		cx = atof(argv[3]), cy = atof(argv[4]);
+	Matx33d K = Matx33d(f, 0, cx,
+		0, f, cy,
+		0, 0, 1);
+	bool is_projective = true;
+	vector<Mat> Rs_est, ts_est, points3d_estimated;
+	reconstruct(images_paths, Rs_est, ts_est, K, points3d_estimated, is_projective);
+	// Print output
+	cout << "\n----------------------------\n" << endl;
+	cout << "Reconstruction: " << endl;
+	cout << "============================" << endl;
+	cout << "Estimated 3D points: " << points3d_estimated.size() << endl;
+	cout << "Estimated cameras: " << Rs_est.size() << endl;
+	cout << "Refined intrinsics: " << endl << K << endl << endl;
+	cout << "3D Visualization: " << endl;
+	cout << "============================" << endl;
+	viz::Viz3d window("Coordinate Frame");
+	window.setWindowSize(Size(500, 500));
+	window.setWindowPosition(Point(150, 150));
+	window.setBackgroundColor(); // black by default
+								 // Create the pointcloud
+	cout << "Recovering points  ... ";
+	// recover estimated points3d
+	vector<Vec3f> point_cloud_est;
+	for (int i = 0; i < points3d_estimated.size(); ++i)
+		point_cloud_est.push_back(Vec3f(points3d_estimated[i]));
+	cout << "[DONE]" << endl;
+	cout << "Recovering cameras ... ";
+	vector<Affine3d> path;
+	for (size_t i = 0; i < Rs_est.size(); ++i)
+		path.push_back(Affine3d(Rs_est[i], ts_est[i]));
+	cout << "[DONE]" << endl;
+	if (point_cloud_est.size() > 0)
+	{
+		cout << "Rendering points   ... ";
+		viz::WCloud cloud_widget(point_cloud_est, viz::Color::green());
+		window.showWidget("point_cloud", cloud_widget);
+		cout << "[DONE]" << endl;
+	}
+	else
+	{
+		cout << "Cannot render points: Empty pointcloud" << endl;
+	}
+	if (path.size() > 0)
+	{
+		cout << "Rendering Cameras  ... ";
+		window.showWidget("cameras_frames_and_lines", viz::WTrajectory(path, viz::WTrajectory::BOTH, 0.1, viz::Color::green()));
+		window.showWidget("cameras_frustums", viz::WTrajectoryFrustums(path, K, 0.1, viz::Color::yellow()));
+		window.setViewerPose(path[0]);
+		cout << "[DONE]" << endl;
+	}
+	else
+	{
+		cout << "Cannot render the cameras: Empty path" << endl;
+	}
+	cout << endl << "Press 'q' to close each windows ... " << endl;
+	window.spin();
 	return 0;
 }

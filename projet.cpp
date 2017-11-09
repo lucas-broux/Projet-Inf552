ec023310f11d11002bef20a9aedd1c7514970073
Romain Loiseau <romain.loiseau@polytechnique.edu>
2017-11-09 15:58:57 +0100

    Json reader fonctionnel

--- projet.cpp
+++ projet.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
+#include "json.hpp"
+#include <fstream>
+
+using json = nlohmann::json;
 using namespace std;
 
 int main()
 {
 	cout << "Hello World" << endl;
 
-	int fin;
-	cin >> fin;
+	// read a JSON file
+	std::ifstream stream_camera("../Files/aachen_000029_000019_test/aachen_000029_000019_camera.json");
+	json camera;
+	stream_camera >> camera;
+	cout << camera.dump() << endl;
+
+	while (true);
 	return 0;
 }

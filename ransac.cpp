046f653a0b2d75b031ae48668d2bcbd30f49dcdc
Romain Loiseau <romain.loiseau@polytechnique.edu>
2017-11-29 10:42:25 +0100

    ransac fit method fonctionnelle

--- ransac.cpp
+++ ransac.cpp
@@ -4,3 +4,11 @@ Ransac::Ransac(int n_iterations, double epsilon) {
 	this->n_iterations = n_iterations;
 	this->epsilon = epsilon;
 };
+
+vector<pair<Vec3d, Vec3b>> Ransac::fit(vector<pair<Vec3d, Vec3b>> pointCloud) {
+	for (int i = 0; i < n_iterations; i++) {
+		//ransac algo
+	}
+
+	return pointCloud;
+};

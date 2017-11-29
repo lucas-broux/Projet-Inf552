f32eaebc8aad597e8a132f99ca7821febb5bc533
Romain Loiseau <romain.loiseau@polytechnique.edu>
2017-11-29 15:15:15 +0100

    class Plan

--- plan.cpp
+++ plan.cpp
@@ -18,10 +18,9 @@ Plan::Plan(double a, double b, double c, double d) {
 /**
 Constructor for the class.
 
-@param a 1st parameter of a*x + b*y + c*z + d = 0
-@param b 2nd parameter of a*x + b*y + c*z + d = 0
-@param c 3rd parameter of a*x + b*y + c*z + d = 0
-@param d 4th parameter of a*x + b*y + c*z + d = 0
+@param p1 1st point of the plan
+@param p2 2nd point of the plan
+@param p3 3rd point of the plan
 */
 Plan::Plan(Vec3d p1, Vec3d p2, Vec3d p3) {
 	this->a = 0;
